a74e39be922298ed0aafdca527c0417e9dda8ce2
octavio.carpes <octavio.carpes10@gmail.com>
2019-03-17 18:04:22 -0300

    Management dashboard header done

--- srm-asset/src/app/views/dashboard-management/dashboard-management.component.ts
+++ srm-asset/src/app/views/dashboard-management/dashboard-management.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-management.component.scss']
 })
 export class DashboardManagementComponent implements OnInit {
-
   constructor() { }
 
+  logo = require('../../../assets/images/blue-globe.png')
+  arrows = require('src/assets/images/down-icon.png')
+  
   ngOnInit() {
   }
 
